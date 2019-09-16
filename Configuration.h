caad268afdecfec6de42fc03beecc49b47c71724
Edward Sandor <ewsandor@gmail.com>
2019-09-15 21:10:54 -0600

    Initial checkin of my customization

--- Configuration.h
+++ Configuration.h
@@ -74,7 +74,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "Jason Driver (Ender-5)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Ed Sandor (Based on Jason Driver)" // Who made the changes.
 //#define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 //#define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
@@ -132,7 +132,7 @@
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "b0d05fb3-c825-48d2-b943-8ff50e9b1e79"
 
 // @section extruder
 
